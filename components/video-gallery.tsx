79fbd157affdbc4eaa66c84422302a37d188666a
Diana Kovacheva <dmarinova@outlook.com>
2025-03-15 15:05:34 +0100

    Add types to the component's props

--- components/video-gallery.tsx
+++ components/video-gallery.tsx
@@ -5,21 +5,27 @@ import HeaderSection from "./movie-details/header-section";
 import VideoList from "./video-list";
 import Video from "./video";
 import Link from "next/link";
+import { VideoGalleryProps } from "../app/actions/videos/definitions";
 
-export default function VideoGallery({ videos, videoListTitle, sectionName }) {
+export default function VideoGallery({
+  videos,
+  videoListTitle,
+  sectionName,
+}: VideoGalleryProps) {
   const [selectedVideo, setSelectedVideo] = useState(videos[0]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [player, setPlayer] = useState(null);
+  const videosCount: number | undefined = videos.length;
 
   const handlePrevious = () => {
-    if (currentIndex > 0) {
+    if (videos && currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
       setSelectedVideo(videos[currentIndex - 1]);
     }
   };
 
   const handleNext = () => {
-    if (currentIndex < videos.length - 1) {
+    if (videos && currentIndex < videos.length - 1) {
       setCurrentIndex(currentIndex + 1);
       setSelectedVideo(videos[currentIndex + 1]);
     }
@@ -31,61 +37,58 @@ export default function VideoGallery({ videos, videoListTitle, sectionName }) {
 
   return videos ? (
     <>
-      {sectionName ? (
-        <HeaderSection sectionName={sectionName} data={videos} />
-      ) : (
-        ""
+      {sectionName && (
+        <HeaderSection sectionName={sectionName} count={Number(videosCount)} />
       )}
 
       {/* Main Content Grid */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
         {/* Main Player Section */}
-        <div className="lg:col-span-2">
-          <div className="relative aspect-video w-full bg-black rounded-xl overflow-hidden">
-            <Video videoId={selectedVideo?.key} onReady={onReady} />
-          </div>
-          <div className="mt-4 space-y-2">
-            <div className="flex gap-3">
-              {selectedVideo?.movie_title ? (
-                <Link
-                  href={`/movie/${
-                    selectedVideo?.movie_id
-                  }-${selectedVideo?.movie_title
-                    .split(" ")
-                    .join("-")
-                    .toLowerCase()}`}
-                  className="flex-none"
-                >
-                  <h3 className="text-xl font-bold text-zinc-500 dark:text-[#f5c518] uppercase line-clamp-2 hover:underline">
-                    {selectedVideo?.movie_title}
-                  </h3>
-                </Link>
-              ) : (
-                ""
-              )}
-
-              <h3 className="text-xl font-medium dark:text-white">
-                {selectedVideo?.name}
-              </h3>
+        {selectedVideo && (
+          <div className="lg:col-span-2">
+            <div className="relative aspect-video w-full bg-black rounded-xl overflow-hidden">
+              <Video videoId={selectedVideo.key} onReady={onReady} />
             </div>
-            <p className="text-sm font-normal text-zinc-500 dark:text-[#c0bcbc]">
-              Video {currentIndex + 1} of {videos.length}
-            </p>
+            <div className="mt-4 space-y-2">
+              <div className="flex gap-3">
+                {selectedVideo.movie_title && (
+                  <Link
+                    href={`/movie/${selectedVideo.movie_id}`}
+                    className="flex-none"
+                  >
+                    <h3 className="text-xl font-bold text-zinc-500 dark:text-[#f5c518] uppercase line-clamp-2 hover:underline">
+                      {selectedVideo.movie_title}
+                    </h3>
+                  </Link>
+                )}
+
+                <h3 className="text-xl font-medium dark:text-white">
+                  {selectedVideo.name}
+                </h3>
+              </div>
+              {videosCount && videosCount > 0 && (
+                <p className="text-sm font-normal text-zinc-500 dark:text-[#c0bcbc]">
+                  Video {currentIndex + 1} of {videosCount}
+                </p>
+              )}
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Videos List Section */}
-        <VideoList
-          videoListTitle={videoListTitle}
-          videos={videos}
-          handlePrevious={handlePrevious}
-          handleNext={handleNext}
-          currentIndex={currentIndex}
-          selectedVideo={selectedVideo}
-          setSelectedVideo={setSelectedVideo}
-          setCurrentIndex={setCurrentIndex}
-          player={player}
-        />
+        {selectedVideo && (
+          <VideoList
+            videoListTitle={videoListTitle}
+            videos={videos}
+            handlePrevious={handlePrevious}
+            handleNext={handleNext}
+            currentIndex={currentIndex}
+            selectedVideo={selectedVideo}
+            setSelectedVideo={setSelectedVideo}
+            setCurrentIndex={setCurrentIndex}
+            player={player}
+          />
+        )}
       </div>
     </>
   ) : (
