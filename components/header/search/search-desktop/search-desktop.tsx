9ea3910b586f3d753eee4b54cfc63e7f0b13ced5
Diana Kovacheva <dmarinova@outlook.com>
2025-03-20 15:39:49 +0100

    Wrap the input field of the search bar with <Suspense>

--- components/header/search/search-desktop/search-desktop.tsx
+++ components/header/search/search-desktop/search-desktop.tsx
@@ -21,18 +21,18 @@ export default function SearchDesktop() {
   }, 300);
 
   return (
-    <Suspense>
-      <div className="join hidden md:flex">
-        <select className="menu dropdown-content bg-base-100 z-1 w-32 p-2 shadow-sm join-item p-2">
-          <option defaultValue="true">All</option>
-          <option>Sci-fi</option>
-          <option>Drama</option>
-          <option>Action</option>
-        </select>
-        <label
-          htmlFor="search"
-          className="input join-item text-black text-base font-medium dark:text-white"
-        >
+    <div className="join hidden md:flex">
+      <select className="menu dropdown-content bg-base-100 z-1 w-32 p-2 shadow-sm join-item p-2">
+        <option defaultValue="true">All</option>
+        <option>Sci-fi</option>
+        <option>Drama</option>
+        <option>Action</option>
+      </select>
+      <label
+        htmlFor="search"
+        className="input join-item text-black text-base font-medium dark:text-white"
+      >
+        <Suspense>
           <input
             type="search"
             placeholder="Search"
@@ -42,23 +42,23 @@ export default function SearchDesktop() {
             }}
             defaultValue={searchParams.get("query")?.toString()}
           />
+        </Suspense>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-[1.5em] w-[1.5em] opacity-50 hidden sm:flex"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </label>
-      </div>
-    </Suspense>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-[1.5em] w-[1.5em] opacity-50 hidden sm:flex"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </label>
+    </div>
   );
 }
