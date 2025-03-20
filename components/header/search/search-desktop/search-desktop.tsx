591e84bc306c8924963b932969dd2419a7bb1db7
Diana Kovacheva <dmarinova@outlook.com>
2025-03-20 15:30:45 +0100

    Wrap calls to useSearchParams() in a Suspense boundary

--- components/header/search/search-desktop/search-desktop.tsx
+++ components/header/search/search-desktop/search-desktop.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { Suspense } from "react";
 import { useDebouncedCallback } from "use-debounce";
 
 export default function SearchDesktop() {
@@ -20,42 +21,44 @@ export default function SearchDesktop() {
   }, 300);
 
   return (
-    <div className="join hidden md:flex">
-      <select className="menu dropdown-content bg-base-100 z-1 w-32 p-2 shadow-sm join-item p-2">
-        <option defaultValue="true">All</option>
-        <option>Sci-fi</option>
-        <option>Drama</option>
-        <option>Action</option>
-      </select>
-      <label
-        htmlFor="search"
-        className="input join-item text-black text-base font-medium dark:text-white"
-      >
-        <input
-          type="search"
-          placeholder="Search"
-          className="grow"
-          onChange={(e) => {
-            handleSearch(e.target.value);
-          }}
-          defaultValue={searchParams.get("query")?.toString()}
-        />
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-[1.5em] w-[1.5em] opacity-50 hidden sm:flex"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+    <Suspense>
+      <div className="join hidden md:flex">
+        <select className="menu dropdown-content bg-base-100 z-1 w-32 p-2 shadow-sm join-item p-2">
+          <option defaultValue="true">All</option>
+          <option>Sci-fi</option>
+          <option>Drama</option>
+          <option>Action</option>
+        </select>
+        <label
+          htmlFor="search"
+          className="input join-item text-black text-base font-medium dark:text-white"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          <input
+            type="search"
+            placeholder="Search"
+            className="grow"
+            onChange={(e) => {
+              handleSearch(e.target.value);
+            }}
+            defaultValue={searchParams.get("query")?.toString()}
           />
-        </svg>
-      </label>
-    </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-[1.5em] w-[1.5em] opacity-50 hidden sm:flex"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </label>
+      </div>
+    </Suspense>
   );
 }
