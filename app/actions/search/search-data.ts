de6feca7a3f15f1d13ec2a8818c22de024189b67
Diana Kovacheva <100419164+dianakovacheva@users.noreply.github.com>
2026-07-28 14:33:21 +0200

    Refactor search function to remove Redis caching and enhance result filtering for movie, TV, and person types

--- app/actions/search/search-data.ts
+++ app/actions/search/search-data.ts
@@ -2,9 +2,6 @@
 
 import baseApiURL from "../API-URLS/base-API-URL";
 import { SearchResponse } from "./types";
-import { redis } from "../../../redis";
-
-const DEFAULT_EXPIRATION = 60 * 60 * 12; // 12 hours
 
 export async function search(
   searchTerm: string,
@@ -13,18 +10,9 @@ export async function search(
   const searchURL = `${baseApiURL}/search/${searchType.trim()}?query=${searchTerm.trim()}&language=en-US&api_key=${
     process.env.API_KEY
   }`;
-  const cacheKey = `search:${searchType}:${searchTerm.toLowerCase()}`;
 
   try {
-    // 1. Try to get from Redis
-    const cached = await redis.get(cacheKey);
-
-    if (cached) {
-      console.log("From Redis Cache");
-      return JSON.parse(cached) as SearchResponse["results"];
-    }
-
-    // 2. Otherwise, fetch from TMDB
+    // Fetch results directly from TMDB
     const res = await fetch(searchURL);
 
     if (!res.ok) {
@@ -33,21 +21,15 @@ export async function search(
 
     const resData: SearchResponse = await res.json();
 
-    // Filter only movie and person results
+    // Filter to movie, tv and person results
     if (resData && resData.results && searchType == "multi") {
       resData.results = resData.results.filter(
         (result) =>
-          result.media_type == "movie" || result.media_type == "person"
+          result.media_type == "movie" ||
+          result.media_type == "tv" ||
+          result.media_type == "person"
       );
-
-      resData.results?.filter((item) => item.title !== undefined);
-      resData.results?.filter((item) => item.name !== undefined);
     }
-
-    // 3. Cache the result in Redis (set TTL)
-    await redis.set(cacheKey, JSON.stringify(resData.results), {
-      EX: DEFAULT_EXPIRATION,
-    });
 
     // Sort results by popularity
     if (resData && resData.results) {
