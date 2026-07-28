3eeff7868a5a66611979dd0f6a5e43cc58b4c11f
Diana Kovacheva <100419164+dianakovacheva@users.noreply.github.com>
2026-07-28 14:26:34 +0200

    Add session provider and enhance page layout with login success, featured series, and top-rated series components

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
 import HeroSection from "../components/hero/hero-holder";
 import FeaturedToday from "../components/featured-today/featured-today";
 import TopRatedMovies from "../components/top-rated-movies";
+import FeaturedSeries from "../components/featured-series";
+import TopRatedSeries from "../components/top-rated-series";
 import MostPopularCelebrities from "../components/most-popular-celebrities";
+import LoginSuccess from "../components/login-success";
+import { SessionProvider } from "next-auth/react";
 
 export default function Page() {
   return (
-    <>
+    <SessionProvider>
+      {/* Display welcome message */}
+      <LoginSuccess />
+
       <HeroSection />
 
       <FeaturedToday />
 
+      <FeaturedSeries />
+
       <MostPopularCelebrities />
 
       <TopRatedMovies />
-    </>
+
+      <TopRatedSeries />
+    </SessionProvider>
   );
 }
