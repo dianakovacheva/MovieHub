8eab24994db35d1e34bf3553195f7c9b0794b009
Diana Kovacheva <100419164+dianakovacheva@users.noreply.github.com>
2026-07-28 14:37:51 +0200

    Refactor WatchlistCarousel component to improve type safety and enhance empty state messaging, ensuring clearer user guidance for managing watchlists.

--- components/watchlist-carousel.tsx
+++ components/watchlist-carousel.tsx
@@ -1,33 +1,33 @@
 import EmptyList from "./empty-list";
 import HeaderSection from "./movie-details/header-section";
 import MoviesCarousel from "./movies-carousel";
+import { MoviesProps } from "../app/actions/movie/definitions";
 
-export default function WatchlistCarousel({ watchlist }: { watchlist?: [] }) {
+export default function WatchlistCarousel({
+  watchlist,
+}: {
+  watchlist?: MoviesProps["movies"];
+}) {
   const sectionName = "Watchlist";
   const listTitle = "No Watchlist yet";
-  const listParagraph = "Create a watchlist to track movies you want to watch.";
+  const listParagraph =
+    "Add movies to your watchlist to keep track of what you want to watch.";
   const buttonText = "Browse popular movies";
-  let watchlistCount: number = 0;
-
-  if (watchlist && watchlist.length) {
-    watchlistCount = watchlist.length;
-  }
+  const watchlistCount = watchlist?.length ?? 0;
 
   return (
     <>
       <HeaderSection sectionName={sectionName} count={watchlistCount} />
-      {watchlist ? (
+      {watchlist && watchlist.length > 0 ? (
         <MoviesCarousel movies={watchlist} />
       ) : (
-        <>
-          <EmptyList
-            listTitle={listTitle}
-            listParagraph={listParagraph}
-            buttonText={buttonText}
-            buttonAction=""
-            className="flex flex-col items-center gap-4 flex-wrap"
-          />
-        </>
+        <EmptyList
+          listTitle={listTitle}
+          listParagraph={listParagraph}
+          buttonText={buttonText}
+          buttonAction="/"
+          className="flex flex-col items-center gap-4 flex-wrap"
+        />
       )}
     </>
   );
