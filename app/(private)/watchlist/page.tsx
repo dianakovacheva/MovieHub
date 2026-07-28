d8b264c8c545712299d74d82c4144a1c5e307871
Diana Kovacheva <100419164+dianakovacheva@users.noreply.github.com>
2026-07-28 14:27:05 +0200

    Implement watchlist page with movie and series carousels, fetching data asynchronously

--- app/(private)/watchlist/page.tsx
+++ app/(private)/watchlist/page.tsx
@@ -1,9 +1,25 @@
 import { Metadata } from "next";
+import WatchlistCarousel from "../../../components/watchlist-carousel";
+import SeriesWatchlistCarousel from "../../../components/series-watchlist-carousel";
+import {
+  getWatchlistMovies,
+  getWatchlistSeries,
+} from "../../actions/watchlist/watchlist-data";
+import PageTitle from "../../../components/page-title";
 
 export const metadata: Metadata = {
   title: "Watchlist",
 };
 
-export default function WatchlistPage() {
-  return "Hello from watchlist page!";
+export default async function WatchlistPage() {
+  const watchlistMovies = await getWatchlistMovies();
+  const watchlistSeries = await getWatchlistSeries();
+
+  return (
+    <div className="flex flex-col gap-4 mb-10">
+      <PageTitle title="Your Watchlist" />
+      <WatchlistCarousel watchlist={watchlistMovies} />
+      <SeriesWatchlistCarousel series={watchlistSeries} />
+    </div>
+  );
 }
