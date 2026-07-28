868407b727cb68b776a7b8393383a354350c9084
Diana Kovacheva <100419164+dianakovacheva@users.noreply.github.com>
2026-07-28 14:35:54 +0200

    Add useAlert hook for context-based alert management, ensuring proper usage within AlertProvider.

---
+++ app/utils/use-alert.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { AlertContext } from "../../context/alert-context";
+import { useContext } from "react";
+
+export const useAlert = () => {
+  const context = useContext(AlertContext);
+
+  if (!context) {
+    throw new Error("useAlert must be used within a AlertProvider");
+  }
+
+  return context;
+};
